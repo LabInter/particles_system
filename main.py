30c8ea088592724ce6ab499d2649d06c734b0e28
Daniel Seitenfus <daniel.seitenfus@combateafraude.com>
2024-10-03 00:29:37 -0300

    feat: change face detection trigger

--- main.py
+++ main.py
@@ -409,6 +409,7 @@ class ParticleSimulation:
     def handle_face_detection(self):
         self.face_detector.face_detected = self.face_detector.process_face_detection()
         if self.face_detector.face_detected:
+            self.wave_movement = False
             self.set_particles_speed(self.particles_speed)
         else:
             self.set_particles_speed(0)
@@ -438,7 +439,7 @@ class ParticleSimulation:
 
             self.screen.blit(self.bg, (0, 0))
 
-            if not self.face_detector.face_detected:
+            if self.wave_movement and not self.face_detector.face_detected:
                 self.handle_face_detection()
 
             if self.should_create_particle():
